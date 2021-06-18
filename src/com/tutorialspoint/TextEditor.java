2795044d4da3e6d1af7191896dabf277bdb24034
PiotrOwczarek1 <PiotrOwczarek1@users.noreply.github.com>
2021-06-18 08:15:40 +0200

    pierwszy commit autowired

---
+++ src/com/tutorialspoint/TextEditor.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TextEditor {
+
+	@Autowired
+	private SpellChecker spellChecker;
+
+	public SpellChecker getSpellChecker() {
+		return spellChecker;
+	}
+
+	public void spellCheck() {
+		spellChecker.checkSpelling();
+	}
+}
